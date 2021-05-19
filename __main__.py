d6b54376de686181b089bfafaadcaf1e998e5421
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-05-19 14:52:39 +0900

    add align_center

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
-from table import Table
 from converter import Converter
+import pandas as pd
+import os
 
-table = Table()
 conveter = Converter()
 
 DATAFOLDER="./datas"
@@ -15,6 +15,17 @@ targets = [
     }
 ]
 
+def align_center(value):
+    # value : string
+    return value.center(os.get_terminal_size().columns)
+
+def display_CSV(csv_path,sort=None):
+    data = pd.read_csv(csv_path)
+    if sort is not None:
+        data = data.sort_values(by=sort["column"] ,ascending=sort["ascending"])
+    print(data.to_string(justify="center"))
+
+
 def __main__():
     conveter.setTargets(targets)
     if not conveter.convert_targets():
@@ -23,8 +34,16 @@ def __main__():
     ## 탐색적 데이터 분석 과정 ##
     # 표를 이용한 분석 과정
     # 1. 대륙별 ipv6 가능 비율 / 2. 대륙 하위 지역별 ipv6가능 비율 (ex : 동아시아와, 서유럽) / 3.국가별 ipv6가능 비율
+    table_names = ["대륙별 ipv6 가능 비율","대륙 하위 지역별 ipv6가능 비율","국가별 ipv6가능 비율"]
     # 1. 대륙별 ipv6 가능 비율
-    
-    
+    # get csv list from csv directory
+    i = 0
+    for csv_name  in os.listdir(CSVFOLDER):
+        path = CSVFOLDER+"/"+csv_name
+        print(align_center("========== {} ==========".format(table_names[i])) )
+        display_CSV(csv_path=path,sort={"column":"IPv6 Capable","ascending":False})
+        i+=1
+
+
 
 __main__()
--- table.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-class Table:
-    def __init__(self):
-        self.pd = pd
-    
-    def display_CSV(self,csv_path):
-        data = self.pd.read_csv(csv_path)
-        print(data)
